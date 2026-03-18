5776b38d3ffde1f3a47cb678e3caef9122894a61
harilakshman-333 <harilakshmanrb@gmail.com>
2026-03-18 23:06:17 +0000

    Fix persistent dependency and UI errors; clean install; ready for deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   turbopack: {
-    root: './'
+    root: '/home/kwalker96/ai_hackathon_edin/diligent-ai'
   }
 };
 
